385f61a60e6ef6d3d2d8827da3579e3357b987b1
KoryNunn <knunn187@gmail.com>
2014-11-27 10:20:33 +1000

    Better error if you try and make Abbott do something usefull

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function checkIfPromise(promise){
+    if(!promise || typeof promise !== 'object' || typeof promise.then !== 'function'){
+        throw "Abbott requires a promise to break. It is the only thing Abbott is good at.";
+    }
+}
+
 module.exports = function abbott(promise){
+    if(typeof promise !== 'function'){
+        checkIfPromise(promise);
+    }
+
     return function(){
         if(typeof promise === 'function'){
            promise = promise.apply(null, Array.prototype.slice.call(arguments, 0, -1));
         }
+        
+        checkIfPromise(promise);
 
         var callback = arguments[arguments.length-1];
         promise.then(callback.bind(null, null), callback);
--- test/index.js
+++ test/index.js
@@ -14,7 +14,7 @@ function makeAStupidPromise(error, data) {
     });
 }
 
-test('Works good', function(t){
+test('Break a promise with success', function(t){
     t.plan(1);
 
     abbott(makeAStupidPromise(null, 10))(function(error, data){
@@ -22,7 +22,7 @@ test('Works good', function(t){
     });
 });
 
-test('Fails good', function(t){
+test('Break a promise with error', function(t){
     t.plan(2);
 
     abbott(makeAStupidPromise('boom', 10))(function(error, data){
@@ -31,7 +31,7 @@ test('Fails good', function(t){
     });
 });
 
-test('promise creator', function(t){
+test('Break a lazily created promise with success', function(t){
     t.plan(1);
 
     abbott(makeAStupidPromise)(null, 10, function(error, data){
@@ -39,11 +39,33 @@ test('promise creator', function(t){
     });
 });
 
-test('Fails good', function(t){
+test('Break a lazily created promise with error', function(t){
     t.plan(2);
 
     abbott(makeAStupidPromise)('boom', 10, function(error, data){
         t.notEqual(data, 10);
         t.equal(error, 'boom');
+    });
+});
+
+test('Attempt to do something other than break a promise', function(t){
+    t.plan(1);
+
+    function aUsefullTask(callback){
+        callback(null, 'usefull result');
+    }
+
+    t.throws(function(){
+        abbott(aUsefullTask)();
+    });
+});
+
+test('Abbott without a promise to break', function(t){
+    t.plan(1);
+
+    t.throws(function(){
+        abbott({'not': 'a promise'})(null, 20, function(error, data){
+            t.fail();
+        });
     });
 });
